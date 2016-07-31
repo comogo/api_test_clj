fc9ae03b7cf09d2d0d08df7fc5248053e0cbddad
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2016-07-31 18:29:05 -0300

    Configure for deploy

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Demo Clojure web API"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.6.1"
+  :uberjar-name "api-standalone.jar"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-json "0.4.0"]
@@ -9,6 +10,6 @@
                  [ring/ring-headers "0.2.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler api.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                 [ring/ring-mock "0.3.0"]]}
+             :production {:env {:production true}}})
